356b195b6fafcfe589fce3f291e63328b83e39e3
pedro-fs-garcia <pedrofsgarcia.pro@gmail.com>
2024-09-01 18:28:35 -0300

    generate from prompt and image

--- src/main/java/com/ollama/testeollama4j/TesteOllama4j.java
+++ src/main/java/com/ollama/testeollama4j/TesteOllama4j.java
@@ -4,12 +4,12 @@
 
 package com.ollama.testeollama4j;
 
+import java.io.File;
 import java.util.List;
 
 import io.github.amithkoujalgi.ollama4j.core.OllamaAPI;
 import io.github.amithkoujalgi.ollama4j.core.models.Model;
 import io.github.amithkoujalgi.ollama4j.core.models.OllamaResult;
-import io.github.amithkoujalgi.ollama4j.core.types.OllamaModelType;
 import io.github.amithkoujalgi.ollama4j.core.utils.OptionsBuilder;
 
 /**
@@ -19,17 +19,53 @@ import io.github.amithkoujalgi.ollama4j.core.utils.OptionsBuilder;
 public class TesteOllama4j {
 
     public static void main(String[] args) throws Exception {
+        String model1 = "llama2";
+        String prompt1 = "why is the Mona Lisa in Paris?";
+        String response1 = generateWithPrompt(model1, prompt1);
+
+        String model2 = "moondream";
+        String prompt2 = "extract the text from this image";
+        String filePath = "/Users/Home/Pictures/ai/003.jpg";
+        String response2 = generateWithImage(model2, prompt2, filePath);
+
+        System.out.println(prompt1);
+        System.out.println(response1);
+        System.out.println("===============");
+        System.out.println(prompt2);
+        System.out.println(response2);
+    }
+
+    public static String generateWithPrompt(String modelName, String prompt) throws Exception {
         String host = "http://localhost:11434/";
         OllamaAPI ollamaAPI = new OllamaAPI(host);
         ollamaAPI.setRequestTimeoutSeconds(60);
         System.out.println("ollama is running: " + ollamaAPI.ping());
+
         List<Model> models = ollamaAPI.listModels();
         System.out.println("models: ");
         models.forEach(model -> System.out.println(model.getName()));
+        
         OptionsBuilder options = new OptionsBuilder();
-        OllamaResult result = ollamaAPI.generate(OllamaModelType.GEMMA2, "who are you?", false, options.build());
-        System.out.println(result.getResponse());
+        options.setTemperature(0.8f);
 
+        OllamaResult result = ollamaAPI.generate(modelName, prompt, false, options.build());
+
+        return result.getResponse();
+    }
+
+    
+    public static String generateWithImage(String modelName, String prompt, String filePath) throws Exception{
+        String host = "http://localhost:11434/";
+        OllamaAPI ollamaAPI = new OllamaAPI(host);
+        ollamaAPI.setRequestTimeoutSeconds(60);
+
+        OptionsBuilder options = new OptionsBuilder();
+        options.setTemperature(0.8f);
+
+        List<File> fileList =  List.of(new File(filePath));
+        OllamaResult result = ollamaAPI.generateWithImageFiles(modelName, prompt, fileList, options.build());
+        String response = result.getResponse();
+        return response;
     }
 
 }
